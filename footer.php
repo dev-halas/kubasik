0aa77a24a51569044b651fd2a92f47b6c88802a6
Krystia Hałas <dev.halas@gmail.com>
2023-10-16 16:41:26 +0200

    create repo

---
+++ footer.php
@@ -0,0 +1,9 @@
+	<footer>
+		<?php wp_footer(); ?>
+		<h1>Footer</h1>
+	</footer>
+</body>
+
+    
+<!-- Javascript-->
+<script src="<?php echo THEME_URL ?>public/bundle.js"></script>
---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+
+	if(!defined('THEME_DIR')) {
+		define('THEME_DIR',get_theme_root().'/'.get_template().'/');
+	}
+
+	if(!defined('THEME_URL')) {
+		define('THEME_URL',WP_CONTENT_URL.'/themes/'.get_template().'/');
+	}
+
+	//--------------Register menus--------------//
+	if(function_exists('register_nav_menus')) { 
+		register_nav_menus (array(
+			'main_nav' => 'Menu glowne',
+		));
+	}
+	
+	require_once THEME_DIR.'libs/post-types.php';
+	require_once THEME_DIR.'libs/utils.php';
+	
+?>
